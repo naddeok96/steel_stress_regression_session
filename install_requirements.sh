1be5f11476efdcca95661059f41a696b00f7bcfe
Kyle Naddeo <naddeok96@github.com>
2023-07-28 16:28:03 -0400

    initial commit

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# BE SURE TO CREATE VENV FIRST
+# python3 -m venv your_virtual_env_name
+
 # Check if the requirements.txt file exists
 if [ -f "requirements.txt" ]; then
     # Install the packages using pip
